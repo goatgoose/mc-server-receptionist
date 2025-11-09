1213196732eaf8029d6584e228a1e9b064d79238
Sam Clark <3758302+goatgoose@users.noreply.github.com>
2025-11-08 21:47:25 -0500

    VarIntString

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use receptionist::Receptionist;
 #[tokio::main]
 async fn main() -> Result<(), io::Error> {
     let receptionist = Receptionist {};
-    receptionist.listen("0.0.0.0:25565").await?;
+    receptionist.listen("localhost:25565").await?;
 
     Ok(())
 }
--- src/protocol.rs
+++ src/protocol.rs
@@ -1,7 +1,7 @@
 use std::io;
 use std::io::Read;
 use serde::{Deserialize, Serialize};
-use crate::codec::VarInt;
+use crate::codec::{VarInt, VarIntString};
 
 #[derive(Debug)]
 pub struct Packet {
@@ -18,8 +18,11 @@ impl Packet {
         let message = match id {
             0x00 => Message::Handshake(Handshake::read_from(reader)?),
             _ => {
-                return Err(io::Error::new(io::ErrorKind::Unsupported, "Unknown packet ID"));
-            }
+                return Err(io::Error::new(
+                    io::ErrorKind::Unsupported,
+                    format!("Unknown packet ID: {:x}", id)
+                ));
+            },
         };
 
         Ok(Packet {
@@ -38,14 +41,17 @@ pub enum Message {
 #[derive(Debug)]
 pub struct Handshake {
     pub protocol_version: i32,
+    pub server_address: String,
 }
 
 impl Handshake {
     pub fn read_from<R: Read>(reader: &mut R) -> io::Result<Self> {
         let protocol_version = i32::from_var_int(reader)?;
+        let server_address = String::from_var_int_string(reader)?;
 
         Ok(Handshake {
             protocol_version,
+            server_address,
         })
     }
 }
